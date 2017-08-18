b1f6c6c6ccc3675e1c5665d493f5c121cb297d65
fumestudioteam <sameh.aly@fumestudio.com>
2017-08-18 14:42:42 +0200

    Initial Commit

---
+++ GNB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GNB
+//
+//  Created by Macintosh HD on 8/18/17.
+//  Copyright © 2017 GNB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
